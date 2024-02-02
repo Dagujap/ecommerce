ac2534415bd56e53e024e2e310b230b922c0a2c5
Diana <dianaprada20@gmail.com>
2024-02-02 11:11:08 -0500

    feature(products): create products app

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    
+    brand = models.CharField(max_length=255)
+    Name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    size = models.IntegerField()
+    measure_unit = models.IntegerField()
+    stock = models.IntegerField()
+    color = models.CharField(max_length=255)
+    status = models.CharField(max_length=10)
